37f2213976010ddbd2651645353f1c1ec68991c3
brandmch <101467867+brandmch@users.noreply.github.com>
2023-01-12 21:14:56 -0500

    spinner added

--- src/App.js
+++ src/App.js
@@ -15,44 +15,54 @@ const randomColor = () => {
   function randomValue() {
     return Math.floor(Math.random() * 256);
   }
-  return `rgb(${randomValue()}, ${randomValue()}, ${randomValue()})`;
+  return `linear-gradient(to bottom right, rgb(${randomValue()}, ${randomValue()}, ${randomValue()}), rgb(255, 255, 255))`;
 };
 
 const App = () => {
-  const [quote, setQuote] = useState({});
+  const [state, setState] = useState({ backgroundColor: "", quote: {} });
+  const [refresh, setRefresh] = useState(false);
 
   useEffect(() => {
-    randomQuote().then((res) => setQuote(res));
-  }, []);
+    randomQuote().then((res) =>
+      setState({ quote: res, backgroundColor: randomColor() })
+    );
+  }, [refresh]);
 
-  console.log(quote);
-
-  return (
-    <div
-      className="App"
-      id="quote-box"
-      style={{ backgroundColor: randomColor() }}
-    >
-      <div className="header">
-        <h1 className="head">Random Quote Generator</h1>
-        <p className="quote" id="text">
-          "{quote.text}"
-        </p>
-        <p className="author" id="author">
-          -{quote.author}
-        </p>
-        <button
-          className="button"
-          id="new-quote"
-          onClick={() => {
-            randomQuote().then((res) => setQuote(res));
-          }}
-        >
-          NewQ
-        </button>
+  if (state.backgroundColor !== "") {
+    return (
+      <div
+        className="App"
+        id="quote-box"
+        style={{ backgroundImage: state.backgroundColor }}
+      >
+        <div className="header">
+          <h1 className="head">Random Quote Generator</h1>
+          <p className="quote" id="text">
+            "{state.quote.text}"
+          </p>
+          <p className="author" id="author">
+            -{state.quote.author}
+          </p>
+          <button
+            className="button"
+            id="new-quote"
+            onClick={() => {
+              setRefresh(!refresh);
+            }}
+          >
+            NewQ
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div class="lds-ripple">
+        <div></div>
+        <div></div>
+      </div>
+    );
+  }
 };
 
 export default App;

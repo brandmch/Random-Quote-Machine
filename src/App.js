a709892c29032abae21d31b5ddb7618810a31b42
brandmch <101467867+brandmch@users.noreply.github.com>
2023-01-12 20:16:12 -0500

    use functiion

--- src/App.js
+++ src/App.js
@@ -1,35 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
-class Quote extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      quote: {},
-    };
-  }
-  render() {
-    return <h1>Swag</h1>;
-  }
-}
-
-function App() {
-  fetch("https://type.fit/api/quotes")
-    .then(function (response) {
+const randomQuote = async () => {
+  return await fetch("https://type.fit/api/quotes")
+    .then((response) => {
       return response.json();
     })
-    .then(function (data) {
-      console.log(data);
+    .then((data) => {
+      return data[Math.floor(Math.random() * data.length)];
     });
+};
+
+const Quote = (props) => {
+  return (
+    <div>
+      <h2>"{props.quote.text}"</h2>
+      <h3>-{props.quote.author}</h3>
+    </div>
+  );
+};
+
+const App = () => {
+  const [quote, setQuote] = useState({});
+
+  useEffect(() => {
+    randomQuote().then((res) => setQuote(res));
+  }, []);
 
   return (
     <div className="App">
       <div className="header">
         <h1>Random Quote Generator</h1>
-        <Quote />
+        <Quote quote={quote} />
+        <button
+          onClick={() => {
+            randomQuote().then((res) => setQuote(res));
+          }}
+        >
+          NewQ
+        </button>
       </div>
     </div>
   );
-}
+};
 
 export default App;
